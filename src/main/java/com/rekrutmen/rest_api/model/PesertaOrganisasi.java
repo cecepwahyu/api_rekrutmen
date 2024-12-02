1a551b4e460da5e28c033f206473344f8fa95c0e
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-02 09:27:37 +0700

    Add model PesertaOrganisasi

---
+++ src/main/java/com/rekrutmen/rest_api/model/PesertaOrganisasi.java
@@ -0,0 +1,34 @@
+package com.rekrutmen.rest_api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "peserta_organisasi")
+@Getter
+@Setter
+public class PesertaOrganisasi {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_org_peserta", nullable = false)
+    private Integer idOrgPeserta;
+
+    @Column(name = "id_peserta", length = 50, nullable = false)
+    private Integer idPeserta;
+
+    @Column(name = "nama_organisasi", length = 100, nullable = false)
+    private String namaOrganisasi;
+
+    @Column(name = "posisi_organisasi", length = 50)
+    private String posisiOrganisasi;
+
+    @Column(name = "periode", length = 50)
+    private String periode;
+
+    @Column(name = "deskripsi_kerja")
+    private String deskripsiKerja;
+}
