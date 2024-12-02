33256bbd4a52df0ef0ad84f399ad961f3bd8f19b
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-02 14:39:26 +0700

    Update register dto

--- src/main/java/com/rekrutmen/rest_api/dto/RegisterRequest.java
+++ src/main/java/com/rekrutmen/rest_api/dto/RegisterRequest.java
@@ -1,52 +1,18 @@
 package com.rekrutmen.rest_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterRequest {
 
     private String username;
     private String nama;
     private String password;
     private String email;
-    private String nik;
 
-    // Getters and Setters
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNik() {
-        return nik;
-    }
-
-    public void setNik(String nik) {
-        this.nik = nik;
-    }
+    @JsonProperty("no_identitas")
+    private String noIdentitas;
 }
