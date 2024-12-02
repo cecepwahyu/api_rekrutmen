90e96020c6742d88662c8072470c31dc15e71787
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-02 16:53:33 +0700

    add riwayat organisasi dto

---
+++ src/main/java/com/rekrutmen/rest_api/dto/PesertaOrganisasiRequest.java
@@ -0,0 +1,22 @@
+package com.rekrutmen.rest_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PesertaOrganisasiRequest {
+
+    @JsonProperty("nama_organisasi")
+    private String namaOrganisasi;
+
+    @JsonProperty("posisi_organisasi")
+    private String posisiOrganisasi;
+
+    @JsonProperty("periode")
+    private String periode;
+
+    @JsonProperty("deskripsi_kerja")
+    private String deskripsiKerja;
+}
