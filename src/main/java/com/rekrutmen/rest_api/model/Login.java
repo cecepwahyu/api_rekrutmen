0ed5d159277d09798414e309592ed86efdf8b736
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-02 14:36:32 +0700

    Update login model

--- src/main/java/com/rekrutmen/rest_api/model/Login.java
+++ src/main/java/com/rekrutmen/rest_api/model/Login.java
@@ -1,42 +1,23 @@
 package com.rekrutmen.rest_api.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Table(name = "tbl_users")
+@Table(name = "tbl_peserta")
+@Getter
+@Setter
 public class Login {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "token")
     private String token;
 
-    public Login() {
-
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
