1db8285a07760a9b67f76d9c79f7bec3b435060a
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-02 16:53:51 +0700

    add riwayat pengalaman kerja dto

---
+++ src/main/java/com/rekrutmen/rest_api/dto/PesertaPengalamanRequest.java
@@ -0,0 +1,22 @@
+package com.rekrutmen.rest_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PesertaPengalamanRequest {
+
+    @JsonProperty("nama_instansi")
+    private String namaInstansi;
+
+    @JsonProperty("posisi_kerja")
+    private String posisiKerja;
+
+    @JsonProperty("periode_kerja")
+    private String periodeKerja;
+
+    @JsonProperty("deskripsi_kerja")
+    private String deskripsiKerja;
+}
