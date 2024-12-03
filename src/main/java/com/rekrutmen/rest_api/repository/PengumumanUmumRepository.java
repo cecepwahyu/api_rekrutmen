d02d79e8c1a832728da4cb285e69d6da549e76ac
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-03 15:06:21 +0700

    add pengumuman repo

---
+++ src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
@@ -0,0 +1,12 @@
+package com.rekrutmen.rest_api.repository;
+
+import com.rekrutmen.rest_api.model.Artikel;
+import com.rekrutmen.rest_api.model.PengumumanUmum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PengumumanUmumRepository extends JpaRepository<PengumumanUmum, UUID> {
+}
