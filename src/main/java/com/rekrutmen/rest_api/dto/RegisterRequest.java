87f41685a62f769adeb9dc3bc119b74d194bccec
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-04 14:24:34 +0700

    add validation register request payload

--- src/main/java/com/rekrutmen/rest_api/dto/RegisterRequest.java
+++ src/main/java/com/rekrutmen/rest_api/dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.rekrutmen.rest_api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,18 +9,33 @@ import lombok.Setter;
 @Setter
 public class RegisterRequest {
 
+    @NotBlank
+    @Pattern(regexp = "^[^\\s]+$",
+            message = "Invalid payload request"
+    )
     @JsonProperty("username")
     private String username;
 
-    @JsonProperty("nama")
-    private String nama;
-
+    @NotBlank
+    @Pattern(
+            regexp = "^[^\\s]+$",
+            message = "Invalid payload request"
+    )
     @JsonProperty("password")
     private String password;
 
+    @NotBlank
+    @Email
+    @Pattern(regexp = "^[^\\s]+$",
+            message = "Invalid payload request"
+    )
     @JsonProperty("email")
     private String email;
 
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{16}$",
+            message = "Invalid payload request"
+    )
     @JsonProperty("no_identitas")
     private String noIdentitas;
 }
