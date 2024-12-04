d3e6eea3093b666c13531f64b590589cd6bf83d2
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-04 14:24:13 +0700

    add validation otp request payload

--- src/main/java/com/rekrutmen/rest_api/dto/OtpVerificationRequest.java
+++ src/main/java/com/rekrutmen/rest_api/dto/OtpVerificationRequest.java
@@ -1,12 +1,22 @@
 package com.rekrutmen.rest_api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.NumberFormat;
 
 @Getter
 @Setter
 public class OtpVerificationRequest {
+
+    @NotBlank
+    @NumberFormat
+    @Pattern(
+            regexp = "^\\d{6}$",
+            message = "Invalid payload request"
+    )
 
     @JsonProperty("otp")
     private String otp;
