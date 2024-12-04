07b5beaa14645345ecac0c3ae13646d5e4a5ff8c
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-04 07:50:53 +0700

    update email service

--- src/main/java/com/rekrutmen/rest_api/service/EmailService.java
+++ src/main/java/com/rekrutmen/rest_api/service/EmailService.java
@@ -16,7 +16,7 @@ public class EmailService {
         message.setTo(toEmail);
         message.setSubject("Your OTP Code");
         message.setText("Your OTP code is: " + otpCode);
-        message.setFrom("seneng.banged@gmail.com");
+        message.setFrom("email@gmail.com");
 
         mailSender.send(message);
     }
