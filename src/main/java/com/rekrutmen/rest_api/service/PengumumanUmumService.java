0d94c49004863411930375bcce9c33ab7ad33847
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 16:41:09 +0700

    add service get pengumuman detail

--- src/main/java/com/rekrutmen/rest_api/service/PengumumanUmumService.java
+++ src/main/java/com/rekrutmen/rest_api/service/PengumumanUmumService.java
@@ -1,14 +1,17 @@
 package com.rekrutmen.rest_api.service;
 
 import com.rekrutmen.rest_api.dto.ResponseWrapper;
+import com.rekrutmen.rest_api.model.Lowongan;
 import com.rekrutmen.rest_api.model.PengumumanUmum;
 import com.rekrutmen.rest_api.repository.PengumumanUmumRepository;
+import com.rekrutmen.rest_api.util.ResponseCodeUtil;
 import com.rekrutmen.rest_api.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PengumumanUmumService {
@@ -19,12 +22,25 @@ public class PengumumanUmumService {
     @Autowired
     private TokenUtil tokenUtil;
 
+    @Autowired
+    private ResponseCodeUtil responseCodeUtil;
+
     public ResponseEntity<ResponseWrapper<List<PengumumanUmum>>> getAllPengumumanUmums(String token) {
+
         // Validate token
         if (!tokenUtil.isValidToken(token)) {
             return ResponseEntity.status(401).body(new ResponseWrapper<>(
-                    "401",
-                    "Unauthorized",
+                    responseCodeUtil.getCode("299"),
+                    responseCodeUtil.getMessage("299"),
+                    null
+            ));
+        }
+
+        // Validate if token is expired
+        if (tokenUtil.isTokenExpired(token)) {
+            return ResponseEntity.status(401).body(new ResponseWrapper<>(
+                    responseCodeUtil.getCode("298"),
+                    responseCodeUtil.getMessage("298"),
                     null
             ));
         }
@@ -32,9 +48,47 @@ public class PengumumanUmumService {
         // Fetch articles
         List<PengumumanUmum> pengumumanUmums = pengumumanUmumRepository.findAll();
         return ResponseEntity.ok(new ResponseWrapper<>(
-                "000",
-                "Success",
+                responseCodeUtil.getCode("000"),
+                responseCodeUtil.getMessage("000"),
                 pengumumanUmums
+        ));
+    }
+
+    public ResponseEntity<ResponseWrapper<PengumumanUmum>> getPengumumanUmumDetail(String token, UUID id) {
+
+        // Validate token
+        if (!tokenUtil.isValidToken(token)) {
+            return ResponseEntity.status(401).body(new ResponseWrapper<>(
+                    responseCodeUtil.getCode("299"),
+                    responseCodeUtil.getMessage("299"),
+                    null
+            ));
+        }
+
+        // Validate if token is expired
+        if (tokenUtil.isTokenExpired(token)) {
+            return ResponseEntity.status(401).body(new ResponseWrapper<>(
+                    responseCodeUtil.getCode("298"),
+                    responseCodeUtil.getMessage("298"),
+                    null
+            ));
+        }
+
+        // Fetch pengumuman umum details
+        PengumumanUmum pengumumanUmum = pengumumanUmumRepository.findById(id).orElse(null);
+
+        if (pengumumanUmum == null) {
+            return ResponseEntity.status(404).body(new ResponseWrapper<>(
+                    responseCodeUtil.getCode("077"),
+                    responseCodeUtil.getMessage("077"),
+                    null
+            ));
+        }
+
+        return ResponseEntity.ok(new ResponseWrapper<>(
+                responseCodeUtil.getCode("000"),
+                responseCodeUtil.getMessage("000"),
+                pengumumanUmum
         ));
     }
 }
