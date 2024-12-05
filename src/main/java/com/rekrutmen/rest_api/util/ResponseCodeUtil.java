15c47b30abdbc4b7f8ddcee79f0de7e29d47847f
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 16:42:08 +0700

    add responsecode in util

--- src/main/java/com/rekrutmen/rest_api/util/ResponseCodeUtil.java
+++ src/main/java/com/rekrutmen/rest_api/util/ResponseCodeUtil.java
@@ -5,10 +5,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ResponseCodeUtil {
+    private Map<String, String> responseCode;
     private Map<String, String> responseMessages;
 
     @PostConstruct
@@ -16,6 +18,10 @@ public class ResponseCodeUtil {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             // Load JSON from resources
+            responseCode = objectMapper.readValue(
+                    getClass().getClassLoader().getResourceAsStream("response_codes.json"),
+                    Map.class
+            );
             responseMessages = objectMapper.readValue(
                     getClass().getClassLoader().getResourceAsStream("response_codes.json"),
                     Map.class
@@ -23,6 +29,10 @@ public class ResponseCodeUtil {
         } catch (IOException e) {
             throw new RuntimeException("Failed to load response_codes.json", e);
         }
+    }
+
+    public String getCode(String code) {
+        return responseCode.containsKey(code) ? code : "Unknown Error Code";
     }
 
     public String getMessage(String code) {
