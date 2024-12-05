22fd6b88ba6c96523ecead4882bc6bb317c8f522
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 10:21:32 +0700

    simplify controller article

--- src/main/java/com/rekrutmen/rest_api/controller/ArtikelController.java
+++ src/main/java/com/rekrutmen/rest_api/controller/ArtikelController.java
@@ -3,7 +3,6 @@ package com.rekrutmen.rest_api.controller;
 import com.rekrutmen.rest_api.dto.ResponseWrapper;
 import com.rekrutmen.rest_api.model.Artikel;
 import com.rekrutmen.rest_api.service.ArtikelService;
-import com.rekrutmen.rest_api.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,28 +19,8 @@ public class ArtikelController {
     @Autowired
     private ArtikelService artikelService;
 
-    @Autowired
-    private TokenUtil tokenUtil;
-
     @GetMapping("/list")
-    public ResponseEntity<ResponseWrapper<List<Artikel>>> getArtikelList(
-            @RequestHeader("Authorization") String token
-    ) {
-        // Validate token
-        if (!tokenUtil.isValidToken(token)) {
-            return ResponseEntity.status(401).body(new ResponseWrapper<>(
-                    "401",
-                    "Unauthorized",
-                    null
-            ));
-        }
-
-        // Fetch articles
-        List<Artikel> artikels = artikelService.getAllArtikels();
-        return ResponseEntity.ok(new ResponseWrapper<>(
-                "000",
-                "Success",
-                artikels
-        ));
+    public ResponseEntity<ResponseWrapper<List<Artikel>>> getArtikelList(@RequestHeader("Authorization") String token) {
+        return artikelService.getArtikelList(token);
     }
 }
