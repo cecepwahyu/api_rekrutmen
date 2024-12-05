223b8b001b8fe708a755f0c1d2d29f365d723e8b
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 10:22:17 +0700

    simplify controller lowongan

--- src/main/java/com/rekrutmen/rest_api/controller/LowonganController.java
+++ src/main/java/com/rekrutmen/rest_api/controller/LowonganController.java
@@ -1,10 +1,8 @@
 package com.rekrutmen.rest_api.controller;
 
 import com.rekrutmen.rest_api.dto.ResponseWrapper;
-import com.rekrutmen.rest_api.model.Artikel;
 import com.rekrutmen.rest_api.model.Lowongan;
 import com.rekrutmen.rest_api.service.LowonganService;
-import com.rekrutmen.rest_api.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,28 +19,8 @@ public class LowonganController {
     @Autowired
     private LowonganService lowonganService;
 
-    @Autowired
-    private TokenUtil tokenUtil;
-
     @GetMapping("/list")
-    public ResponseEntity<ResponseWrapper<List<Lowongan>>> getLowonganList(
-            @RequestHeader("Authorization") String token
-    ) {
-        // Validate token
-        if (!tokenUtil.isValidToken(token)) {
-            return ResponseEntity.status(401).body(new ResponseWrapper<>(
-                    "401",
-                    "Unauthorized",
-                    null
-            ));
-        }
-
-        // Fetch lowongan list
-        List<Lowongan> lowongans = lowonganService.getAllLowongans();
-        return ResponseEntity.ok(new ResponseWrapper<>(
-                "000",
-                "Success",
-                lowongans
-        ));
+    public ResponseEntity<ResponseWrapper<List<Lowongan>>> getLowonganList(@RequestHeader("Authorization") String token) {
+        return lowonganService.getLowonganList(token);
     }
 }
