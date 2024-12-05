aa9197f4a8e7e53be6f5c9e8e48e8187c28c7cb0
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 16:43:25 +0700

    improve pengumuman repo

--- src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
@@ -5,8 +5,10 @@ import com.rekrutmen.rest_api.model.PengumumanUmum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PengumumanUmumRepository extends JpaRepository<PengumumanUmum, UUID> {
+    Optional<PengumumanUmum> findById(UUID id);
 }
