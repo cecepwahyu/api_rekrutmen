63d7c31f02b1ae7cb87ad928c2187fadf33fc878
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 16:39:58 +0700

    add endpoint get pengumuman detail

--- src/main/java/com/rekrutmen/rest_api/controller/PengumumanUmumController.java
+++ src/main/java/com/rekrutmen/rest_api/controller/PengumumanUmumController.java
@@ -1,17 +1,16 @@
 package com.rekrutmen.rest_api.controller;
 
 import com.rekrutmen.rest_api.dto.ResponseWrapper;
+import com.rekrutmen.rest_api.model.Artikel;
 import com.rekrutmen.rest_api.model.PengumumanUmum;
 import com.rekrutmen.rest_api.service.PengumumanUmumService;
 import com.rekrutmen.rest_api.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/pengumuman-umum")
@@ -26,5 +25,12 @@ public class PengumumanUmumController {
     @GetMapping("/list")
     public ResponseEntity<ResponseWrapper<List<PengumumanUmum>>> getPengumumanUmumList(@RequestHeader("Authorization") String token) {
         return pengumumanUmumService.getAllPengumumanUmums(token);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseWrapper<PengumumanUmum>> getPengumumanUmumDetail(
+            @RequestHeader("Authorization") String token,
+            @PathVariable UUID id) {
+        return pengumumanUmumService.getPengumumanUmumDetail(token, id);
     }
 }
