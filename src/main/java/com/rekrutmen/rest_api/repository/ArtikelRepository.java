5254b83d3dda67d8287cf2209bbebeec18b65187
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-05 16:43:03 +0700

    improve artikel repo

--- src/main/java/com/rekrutmen/rest_api/repository/ArtikelRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/ArtikelRepository.java
@@ -1,11 +1,14 @@
 package com.rekrutmen.rest_api.repository;
 
 import com.rekrutmen.rest_api.model.Artikel;
+import com.rekrutmen.rest_api.model.Lowongan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ArtikelRepository extends JpaRepository<Artikel, UUID> {
+    Optional<Artikel> findById(UUID id);
 }
