9625bc70664eab7ed68776ace3383192d47f17d1
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-20 08:29:24 +0700

    improve repo with add findbyIdPeserta

--- src/main/java/com/rekrutmen/rest_api/repository/PesertaPendidikanRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/PesertaPendidikanRepository.java
@@ -3,6 +3,9 @@ package com.rekrutmen.rest_api.repository;
 import com.rekrutmen.rest_api.model.PesertaPendidikan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PesertaPendidikanRepository extends JpaRepository<PesertaPendidikan, Integer> {
+    Optional<PesertaPendidikan> findByIdPeserta(Integer idPeserta);
     void deleteByIdPeserta(Integer idPeserta); // Use the correct field name
 }
