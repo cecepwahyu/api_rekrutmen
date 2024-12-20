b8a58b23d5db4cb815ae035389839d967eb3d6e6
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-20 08:29:55 +0700

    remove unused import

--- src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
@@ -1,6 +1,5 @@
 package com.rekrutmen.rest_api.repository;
 
-import com.rekrutmen.rest_api.model.Artikel;
 import com.rekrutmen.rest_api.model.PengumumanUmum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
