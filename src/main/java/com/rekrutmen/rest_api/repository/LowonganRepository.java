e2056b913148c75d4548a34e5b12f38d074af7f3
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-25 20:13:10 +0700

    add find by slug in repo

--- src/main/java/com/rekrutmen/rest_api/repository/LowonganRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/LowonganRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface LowonganRepository extends JpaRepository<Lowongan, Integer> {
     Optional<Lowongan> findByIdLowongan(Long idLowongan);
+    Optional<Lowongan> findBySlug(String slug);
 }
