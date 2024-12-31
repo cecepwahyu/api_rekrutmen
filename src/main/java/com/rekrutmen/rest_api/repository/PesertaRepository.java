5d5f982ec1843334f158fbf75df37e9d26467bc8
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-31 08:20:45 +0700

    improve peserta repo

--- src/main/java/com/rekrutmen/rest_api/repository/PesertaRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/PesertaRepository.java
@@ -1,7 +1,10 @@
 package com.rekrutmen.rest_api.repository;
 
+import com.rekrutmen.rest_api.dto.PesertaInfoRequest;
 import com.rekrutmen.rest_api.model.Peserta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,9 +14,13 @@ public interface PesertaRepository extends JpaRepository<Peserta, Long> {
     Optional<Peserta> findByEmailAndNoIdentitas(String email, String noIdentitas);
     Optional<Peserta> findByToken(String token);
     Optional<Peserta> findByOtp(String otp);
+    Optional<Peserta> findByTelp(String telp);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByTelp(String telp);
     boolean existsByNoIdentitas(String noIdentitas);
+
+    @Query("SELECT new com.rekrutmen.rest_api.dto.PesertaInfoRequest(p.nama, p.email, p.noPeserta) FROM Peserta p WHERE p.idPeserta = :idPeserta")
+    Optional<PesertaInfoRequest> findPesertaInfoByIdPeserta(@Param("idPeserta") Integer idPeserta);
 }
 
