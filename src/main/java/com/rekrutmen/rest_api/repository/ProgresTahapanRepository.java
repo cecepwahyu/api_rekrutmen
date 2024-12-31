e9775b74db9d06146b8763ea9d9bbbf49d665a61
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2024-12-31 08:16:07 +0700

    add progress repo

--- src/main/java/com/rekrutmen/rest_api/repository/ProgresTahapanRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/ProgresTahapanRepository.java
@@ -1,6 +1,5 @@
 package com.rekrutmen.rest_api.repository;
 
-import com.rekrutmen.rest_api.model.Lowongan;
 import com.rekrutmen.rest_api.model.ProgresTahapan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
