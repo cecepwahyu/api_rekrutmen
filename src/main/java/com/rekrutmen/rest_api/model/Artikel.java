b140f7f3e9613ea28d90f3bb5ff45da94dea5dd6
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2025-01-09 08:53:55 +0700

    update model Artikel

--- src/main/java/com/rekrutmen/rest_api/model/Artikel.java
+++ src/main/java/com/rekrutmen/rest_api/model/Artikel.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class Artikel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false, length = 100)
