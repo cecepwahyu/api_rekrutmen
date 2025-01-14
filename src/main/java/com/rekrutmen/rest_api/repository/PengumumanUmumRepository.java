899b2d0a0b348889aef039498ece45d222e5552c
cecep.cahyana <cecep.cahyana@bpddiy.co.id>
2025-01-14 08:30:22 +0700

    add query filtering pengumuman publish

--- src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
+++ src/main/java/com/rekrutmen/rest_api/repository/PengumumanUmumRepository.java
@@ -1,7 +1,10 @@
 package com.rekrutmen.rest_api.repository;
 
 import com.rekrutmen.rest_api.model.PengumumanUmum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,8 @@ import java.util.UUID;
 public interface PengumumanUmumRepository extends JpaRepository<PengumumanUmum, UUID> {
     Optional<PengumumanUmum> findById(UUID id);
     Optional<PengumumanUmum> findBySlug(String slug);
+
+    @Query("SELECT p FROM PengumumanUmum p WHERE p.statusPublish = '1' AND p.approved IS NOT NULL AND p.approved = true ORDER BY p.createdAt DESC")
+    Page<PengumumanUmum> findPublishedAndApproved(Pageable pageable);
+
 }
